30563ba40ca9175d4142273bc27c274478f14540
Kamil <83810912+Milten1@users.noreply.github.com>
2021-06-22 09:53:54 +0200

    Add files via upload

---
+++ Logic.java
@@ -0,0 +1,98 @@
+
+package com.mycompany.chess;
+
+import Pieces.*;
+
+public class Logic {
+    private Pieces[][] logicBoard;
+    private Board board;
+    private String player;
+    private String enemy;
+    private boolean changePlayer;
+    
+    public Logic() {
+        this.logicBoard = new Pieces[8][8];
+        this.board = new Board();
+        this.changePlayer = false;
+        this.player = "White";
+    }
+    
+    public Pieces[][] move(String command){
+        int[] coordinates = convertCommandToCoordinates(command);
+        Pieces piece = logicBoard[coordinates[0]][coordinates[1]];
+        logicBoard = board.getBoard();
+
+        changePlayer = false;
+        
+        logicBoard[coordinates[0]][coordinates[1]] = new Empty();
+        logicBoard[coordinates[2]][coordinates[3]] = piece;
+        
+        board.printBoard();
+        
+        changePlayer = true;
+        
+        
+        return logicBoard;
+    }
+    
+    public boolean isEndOfTheGame(){
+        return false;
+    }
+    
+    public boolean getChangePlayer(){
+        return this.changePlayer;
+    }
+    
+    public void changePlayer(){
+        
+    }
+    
+    public int[] convertCommandToCoordinates(String command){
+        
+        
+        //coordinates format example; a3-d4
+        
+        String[] commands = command.split("-");
+        
+        String[] cmd1 = commands[0].split("");
+        String[] cmd2 = commands[1].split("");
+
+
+        int[] coordinates = new int[4];
+        
+        coordinates[1] = letterToNumber(cmd1[0])-1;
+        coordinates[0] = Integer.valueOf(cmd1[1])-1;
+        coordinates[3] = letterToNumber(cmd2[0])-1;
+        coordinates[2] = Integer.valueOf(cmd2[1])-1;
+        
+        
+        return coordinates;
+    }
+    
+    public int letterToNumber(String letter){
+        switch(letter){
+            case "a": return 1;
+            case "b": return 2;
+            case "c": return 3;
+            case "d": return 4;
+            case "e": return 5;
+            case "f": return 6;
+            case "g": return 7;
+            case "h": return 8;
+        }
+        return 0;
+    }
+    
+
+    public void saveGame(String name) {
+        
+    }
+
+    public void loadGame(String name) {
+        
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+}
